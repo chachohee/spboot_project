27aff289b47cbfb934d7512a95f03f2abbeac005
chachohee <cxoxee@gmail.com>
2023-08-14 00:08:47 +0900

    아이디, 비밀번호 찾기 기능 추가

--- src/main/java/com/example/project/service/MemberService.java
+++ src/main/java/com/example/project/service/MemberService.java
@@ -1,6 +1,7 @@
 package com.example.project.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import com.example.project.dao.MemberDao;
@@ -10,40 +11,48 @@ import com.example.project.vo.ResultData;
 
 @Service
 public class MemberService {
-	
+
+	@Value("${custom.siteName}")
+	private String siteName;
+	@Value("${custom.siteMainUri}")
+	private String siteMainUri;
+
 	private MemberDao memberDao;
+	private MailService mailService;
 
 	@Autowired
-	MemberService(MemberDao memberDao){
+	MemberService(MemberDao memberDao, MailService mailService) {
 		this.memberDao = memberDao;
+		this.mailService = mailService;
 	}
 
-public ResultData<Member> doJoin(String loginId, String loginPw, String name, String nickname, String cellphoneNum, String email) {
-		
+	public ResultData<Member> doJoin(String loginId, String loginPw, String name, String nickname, String cellphoneNum,
+			String email) {
+
 		Member existsMember = getMemberByLoginId(loginId);
-		
+
 		if (existsMember != null) {
 			return ResultData.from("F-7", Util.f("이미 사용중인 아이디(%s)입니다.", loginId));
 		}
-		
+
 		existsMember = getMemberByNickname(nickname);
-		
+
 		if (existsMember != null) {
 			return ResultData.from("F-8", Util.f("이미 사용중인 닉네임(%s)입니다.", nickname));
 		}
-		
+
 		existsMember = getMemberByNameAndEmail(name, email);
-		
+
 		if (existsMember != null) {
 			return ResultData.from("F-9", Util.f("이미 사용중인 이름(%s)과 이메일(%s)입니다.", name, email));
 		}
-		
+
 		memberDao.doJoin(loginId, loginPw, name, nickname, cellphoneNum, email);
-		
+
 		return ResultData.from("S-1", Util.f("%s님이 가입되었습니다.", nickname), "member", getMemberById(getLastInsertId()));
 	}
-	
-	private Member getMemberByNameAndEmail(String name, String email) {
+
+	public Member getMemberByNameAndEmail(String name, String email) {
 		return memberDao.getMemberByNameAndEmail(name, email);
 	}
 
@@ -69,6 +78,26 @@ public ResultData<Member> doJoin(String loginId, String loginPw, String name, St
 
 	public void doPasswordModify(int loginedMemberId, String loginPw) {
 		memberDao.doPasswordModify(loginedMemberId, loginPw);
-		
+	}
+	
+	public ResultData notifyTempLoginPwByEmail(Member member) {
+		String title = "[" + siteName + "] 임시 패스워드 발송";
+		String tempPassword = Util.getTempPassword(8);
+		String body = "<h1>임시 패스워드 : " + tempPassword + "</h1>";
+		body += "<a style='font-size:4rem;' href=\"" + siteMainUri + "/usr/member/login\" target=\"_blank\">로그인 하러가기</a>";
+
+		ResultData sendRd = mailService.send(member.getEmail(), title, body);
+
+		if (sendRd.isFail()) {
+			return sendRd;
+		}
+
+		setTempPassword(member, tempPassword);
+
+		return ResultData.from("S-1", "계정의 이메일주소로 임시 패스워드가 발송되었습니다.");
+	}
+
+	private void setTempPassword(Member member, String tempPassword) {
+		doPasswordModify(member.getId(), Util.sha256(tempPassword));
 	}
 }
--- src/main/webapp/WEB-INF/jsp/usr/member/findLoginPw.jsp
+++ src/main/webapp/WEB-INF/jsp/usr/member/findLoginPw.jsp
@@ -1,8 +1,8 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
-	pageEncoding="UTF-8"%>
+    pageEncoding="UTF-8"%>
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
 
-<c:set var="pageTitle" value="로그인" />
+<c:set var="pageTitle" value="비밀번호 찾기" />
 
 <%@ include file="../common/head.jsp"%>
 
@@ -11,7 +11,7 @@
 		<div
 			class="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100 mx-auto">
 			<div class="card-body">
-				<form action="doLogin" method="post">
+				<form action="doFindLoginPw" method="POST">
 					<div class="form-control">
 						<label class="label"> 
 							<span class="label-text">Id</span>
@@ -23,18 +23,24 @@
 					</div>
 					<div class="form-control">
 						<label class="label"> 
-							<span class="label-text">Password</span>
+							<span class="label-text">Name</span>
 						</label> 
-						<input type="password" name="loginPw" placeholder="password"
-							class="input input-bordered" /> 
-						<label class="label"> 
-							<a href="findLoginPw" class="label-text-alt link link-hover">Forgot password?</a>
-						</label>
+						<input type="text" name="name" placeholder="name" class="input input-bordered" />
 					</div>
-					<div class="form-control mt-6">
-						<button class="btn btn-primary">Login</button>
+					<div class="form-control">
+						<label class="label"> 
+							<span class="label-text">Email</span>
+						</label> 
+						<input type="text" name="email" placeholder="email"
+							class="input input-bordered" /> 
+					</div>
+					<div class="form-control mt-4">
+						<button class="btn btn-primary">비밀번호 찾기</button>
 					</div>
 				</form>
+			</div>
+			<div class="mx-auto mb-8">
+				<a class="btn btn-sm" href="login">로그인</a>
 			</div>
 		</div>
 		<div>
